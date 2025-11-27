9eb8f10cb268abd66487b4c37abf5a91d105d7eb
iunoo2 <syahbandi.bbn@gmail.com>
2025-11-27 11:18:31 +0700

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -15,13 +15,7 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS
   : ['http://localhost:5173'];
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: true,
   credentials: true
 }));
 app.use(express.json());
